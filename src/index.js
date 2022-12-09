1945bfe128359782caa73ee086d8a8412c2f9e62
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2022-12-09 11:49:36 +0430

    add insert, delete and  update functions

--- src/index.js
+++ src/index.js
@@ -1,55 +1,102 @@
+// const { remove } = require('lodash');
 require('./style.css');
 
 const inputText = document.getElementById('input');
-
+// const clearBtn = document.getElementById('clear');
+const tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
 const listcontainer = document.getElementById('listcontainer');
 
-const todoArray = [
-  {
-    text: 'first',
-    index: 1,
-    complete: false,
-  },
-  {
-    text: 'second',
-    index: 2,
-    complete: false,
-  },
-  {
-    text: 'Third',
-    index: 3,
-    complete: false,
-  },
-];
+class Task {
+  constructor(description, index) {
+    this.description = description;
+    this.completed = false;
+    this.index = index;
+  }
+}
 
-// inputText.addEventListener('keypress', (e) => {
-//     if(inputText.value !== '' && e.key == 'Enter'){
-//     todoArray.push({
-//         text: inputText.value,
-//         index: todoArray.length,
-//         complete: false,
-//     })
-//     show();
-// }
-//   });
-
-const show = () => {
-  listcontainer.innerHTML = '';
-  todoArray.forEach((element) => {
-    if (element.index !== 0) {
-      const li = document.createElement('li');
-      li.setAttribute('id', 'dynamicList');
-
-      const check = document.createElement('input');
-      check.type = 'checkbox';
-      check.name = element.index;
-
-      const lbl = document.createElement('label');
-      lbl.innerHTML = element.text;
-      li.append(lbl, check);
-      listcontainer.append(li);
-    }
-    inputText.value = '';
-  });
+const addTask = (description) => {
+  const task = new Task(description, tasks.length + 1);
+  const newTasks = [...tasks, task];
+  localStorage.setItem('tasks', JSON.stringify(newTasks));
+  document.location.reload();
 };
-show();
+
+inputText.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    const description = inputText.value;
+    addTask(description);
+    // alert('fdjlaskfl');
+  }
+});
+
+const removeTask = (index) => {
+  let newTasks = tasks.filter((task) => task.index !== index);
+  newTasks = newTasks.map((task, index) => {
+    task.index = index + 1;
+    return task;
+  });
+  localStorage.setItem('tasks', JSON.stringify(newTasks));
+  document.location.reload();
+};
+
+const updateTask = (index, editedText) => {
+  const newTasks = tasks.filter((item) => item.index !== index);
+
+  const newTask = new Task(editedText.value, index);
+  newTasks.splice(index - 1, 0, { ...newTask });
+
+  localStorage.setItem('tasks', JSON.stringify(newTasks));
+  document.location.reload();
+};
+
+const editTask = (index) => {
+  const task = tasks[index - 1];
+  const item = document.getElementById(index);
+  item.innerHTML = '';
+  const editedText = document.createElement('input');
+  editedText.classList.add('editedText');
+  editedText.value = task.description;
+
+  editedText.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      updateTask(index, editedText);
+    }
+  });
+  const menuOk = document.createElement('i');
+  menuOk.classList.add('fas');
+  menuOk.classList.add('fa-trash');
+  menuOk.classList.add('menuOk');
+  menuOk.addEventListener('click', () => {
+    removeTask(index);
+  });
+
+  item.append(editedText, menuOk);
+};
+
+tasks.forEach((element) => {
+  // if (element.index !== 0) {
+  const li = document.createElement('li');
+  li.setAttribute('id', element.index);
+  li.classList.add('dynamicList');
+
+  const check = document.createElement('input');
+  check.setAttribute('type', 'checkbox');
+
+  const todoText = document.createElement('label');
+  todoText.innerHTML = element.description;
+
+  const listItemIcon = document.createElement('i');
+  listItemIcon.classList.add('listItemIcon');
+  listItemIcon.classList.add('fas');
+  listItemIcon.classList.add('fa-ellipsis-v');
+
+  listItemIcon.addEventListener('click', () => {
+    editTask(element.index);
+  });
+
+  li.append(check, todoText, listItemIcon);
+  listcontainer.append(li);
+  //   }
+  //   inputText.value = '';
+  // });
+});
